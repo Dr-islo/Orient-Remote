9693d7c715fb88368ff3be9e903eccef818ad771
Dr-islo <rehanali.github@gmail.com>
2026-07-05 13:07:41 +0500

    Delete OrientRemote directory

--- OrientRemote/app/src/main/java/com/orientremote/app/util/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.orientremote.app.util
-
-object Constants {
-    /** Repeat interval while a repeatable button (volume/channel) is held down. */
-    const val LONG_PRESS_REPEAT_INTERVAL_MS = 250L
-
-    /** Buttons become "repeatable" (hold-to-repeat) after this initial delay. */
-    const val LONG_PRESS_INITIAL_DELAY_MS = 350L
-}
